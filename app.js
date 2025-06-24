577290fa7fc2c9ae8f38b0bbbcdca67fd769b518
thisisjose <josecinuwu@gmail.com>
2025-06-24 00:04:18 -0700

    función me gusta

--- app.js
+++ app.js
@@ -1,43 +1,52 @@
 const canciones = [
+    //OBJE LIST
   {
     imagen: 'imgs/album1.jpg',
     artista: 'THE BEATLES',
-    titulo: 'Lucy in the Sky with Diamonds'
+    titulo: 'Lucy in the Sky with Diamonds',
+    like: false
   },
   {
     imagen: 'imgs/album2.jpg',
     artista: 'NIRVANA',
-    titulo: 'Come as You are'
+    titulo: 'Come as You are',
+    like: false
   },
   {
     imagen: 'imgs/album3.jpg',
     artista: 'MAC DEMARCO',
-    titulo: 'My kind of Woman'
+    titulo: 'My kind of Woman',
+    like: false
   },
   {
     imagen: 'imgs/album4.jpg',
     artista: 'IRON MAIDEN',
-    titulo: 'Chuchuwa'
+    titulo: 'Chuchuwa',
+    like: false
   },
   {
     imagen: 'imgs/album5.jpg',
     artista: 'RADIOHEAD',
-    titulo: 'Paranoid Android'
+    titulo: 'Paranoid Android',
+    like: false
   },
   {
     imagen: 'imgs/album6.jpg',
     artista: 'QUEEN',
-    titulo: 'Bohemian Rhapsody'
+    titulo: 'Bohemian Rhapsody',
+    like: false 
   },
   {
     imagen: 'imgs/album7.jpg',
     artista: 'GREEN DAY',
-    titulo: 'American Idiot'
+    titulo: 'American Idiot',
+    like: false
   },
   {
     imagen: 'imgs/album8.jpg',
     artista: 'CUCO',
-    titulo: 'Lover is a Day'
+    titulo: 'Lover is a Day',
+    like: false
   }
 ];
 
@@ -56,8 +65,14 @@ function mostrarCancion(index) {
   tituloActual.textContent = cancion.titulo;
   artistaActual.textContent = cancion.artista;
   indiceActual = index;
-}
 
+//   Actualizar el estado del botón de "Me gusta"
+  if (cancion.like) {
+    btnLike.classList.add('liked');
+  } else {
+    btnLike.classList.remove('liked');
+  }
+}
 // CAMBIA CANCION CON CLICK EN IMAG
 albumes.forEach(album => {
   album.addEventListener('click', () => {
@@ -76,4 +91,12 @@ btnSiguiente.addEventListener('click', () => {
 btnAnterior.addEventListener('click', () => {
   let anterior = (indiceActual - 1 + canciones.length) % canciones.length;
   mostrarCancion(anterior);
+});
+
+//BOTON ME GUSTA
+const btnLike = document.getElementById('btn-like');
+
+btnLike.addEventListener('click', function () {
+  canciones[indiceActual].like = !canciones[indiceActual].like;
+  btnLike.classList.toggle('liked', canciones[indiceActual].like);
 });
